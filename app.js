79f91663c4e8cc0c939b125c10b453738151835b
Erali <eralitemirov129@gmail.com>
2022-11-26 11:54:49 +0500

    w

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-let son=+prompt("Son kiriting");
-let Summa=0;
-for(let i=1; i<=son; i++){
-  Summa = Summa+i;
-}
+for(; ;){
+  let son = +prompt("Son kiriting");
+  let Summa = 0;
+  for (let i = 1; i <= son; i++) {
+    Summa = Summa + i;
+  }
 
-alert("Yig'indi:" +Summa);
+  alert("Yig'indi:" + Summa);
+
+}
